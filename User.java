9708cbe15311a67495bab0e63e6e0c70983e0b48
Tahscene <125144053+Tahscene@users.noreply.github.com>
2023-08-12 21:57:57 +0600

    Add files via upload

---
+++ User.java
@@ -0,0 +1,87 @@
+package model;
+
+public class User {
+
+    private int id;
+    private String email;
+    private String name;
+    private String no;
+    private String add;
+    private String pass;
+    private String ques;
+    private String ans;
+    private String status;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNo() {
+        return no;
+    }
+
+    public void setNo(String no) {
+        this.no = no;
+    }
+
+    public String getAdd() {
+        return add;
+    }
+
+    public void setAdd(String add) {
+        this.add = add;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    public String getQues() {
+        return ques;
+    }
+
+    public void setQues(String ques) {
+        this.ques = ques;
+    }
+
+    public String getAns() {
+        return ans;
+    }
+
+    public void setAns(String ans) {
+        this.ans = ans;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+}
